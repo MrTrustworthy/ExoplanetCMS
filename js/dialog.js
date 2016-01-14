a70043b33e2e50cb2e7850cac53dfd8a368cc3ff
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2016-01-14 22:48:14 +0100

    dialog buttons first version

--- js/dialog.js
+++ js/dialog.js
@@ -20,6 +20,7 @@ var Dialog = function (body) {
     document.body.appendChild(document.importNode(dialog_template, true));
 
     this.dlg = document.getElementById(dialog_id);
+    this.generate_content();
 
     // cancel on rightclick
     this.dlg.oncontextmenu = function () {
@@ -56,82 +57,63 @@ Dialog.prototype.close = function () {
     var deferred = new Deferred();
     this.dlg.classList.remove("body_dialog_open");
     this.dlg.classList.add("body_dialog_closed");
-    window.setTimeout(function(){
+    window.setTimeout(function () {
         this.dlg.close();
         deferred.resolve();
     }.bind(this), 1000);
     return deferred.promise;
 };
 
+Dialog.prototype.generate_content = function () {
+
+    /**
+     * LEFT BOX
+     */
+
+    var left_box = this.dlg.querySelector(".dialog_content_left");
+
+    var img = document.createElement("img");
+    img.src = "/graphics/arrow_left.png";
+
+    img.onclick = function () {
+        var controller = require("js/main").controller;
+        if(this.body.parent) controller.select_object(this.body.parent);
+        else controller.cancel_selection();
+    }.bind(this);
+
+    left_box.appendChild(img);
+
+    /**
+     * CENTER BOX
+     */
+    var center_box = this.dlg.querySelector(".dialog_content_middle");
+
+    var header = document.createElement("h1");
+    header.innerHTML = this.body.info.title;
+    center_box.appendChild(header);
+
+    var content = document.createElement("p");
+    content.innerHTML = this.body.info.content;
+    center_box.appendChild(content);
+
+    /**
+     * RIGHT BOX
+     */
+    var right_box = this.dlg.querySelector(".dialog_content_right");
+
+    this.body.info.children.forEach(function (child) {
+
+        var img = document.createElement("img");
+        img.src = "/graphics/arrow_right.png";
+        img.onclick = function () {
+            require("js/main").controller.select_object(child.backref);
+        };
+
+        right_box.appendChild(img);
+
+    }.bind(this));
+
+};
+
+
 module.exports = Dialog;
-
-
-//Dialog.prototype.generate_content = function () {
-//
-//    var container = document.createElement("div");
-//    container.className = "dialog_container";
-//    this.dlg.appendChild(container);
-//
-//
-//    // create the 3 main containers and fill them
-//    var dialog_content_container = document.createElement("div");
-//    dialog_content_container.className = "dialog_content_container";
-//    container.appendChild(dialog_content_container);
-//
-//    var dialog_back_container = document.createElement("div");
-//    dialog_back_container.className = "dialog_back_container";
-//    container.appendChild(dialog_back_container);
-//
-//    var dialog_forward_container = document.createElement("div");
-//    dialog_forward_container.className = "dialog_forward_container";
-//    container.appendChild(dialog_forward_container);
-//
-//
-//    // generate center elements
-//    var header = document.createElement("h1");
-//    header.innerHTML = this.body.info.title;
-//    dialog_content_container.appendChild(header);
-//
-//    var content = document.createElement("div");
-//    content.innerHTML = this.body.info.content;
-//    dialog_content_container.appendChild(content);
-//
-//    //generate back arrows
-//    if (this.body.parent) {
-//        dialog_back_container.innerHTML = this.body.parent.info.title;
-//        dialog_back_container.onclick = window.controller.select_object.bind(window.controller, this.body.parent);
-//    } else {
-//        dialog_back_container.innerHTML = "k";
-//        dialog_back_container.onclick = window.controller.cancel_selection.bind(window.controller);
-//    }
-//
-//    //generate forward arrows
-//    this.body.info.children.forEach(function (child) {
-//        button = document.createElement("button");
-//        button.innerHTML = "Go to " + child.title;
-//        button.onclick = window.controller.select_object.bind(window.controller, child.backref);
-//        dialog_forward_container.appendChild(button);
-//    }.bind(this));
-//
-//};
-
-
-/**
- // *
- // * @param new_pos
- // */
-//Dialog.prototype.update_position = function (new_pos) {
-//    console.log("update dlg pos", new_pos);
-//
-//    this.dlg.style.top = new_pos.y + "px";
-//    this.dlg.style.left = new_pos.x + "px";
-//};
-
-
-///**
-// *
-// * @param to
-// */
-//Dialog.prototype.fade_to = function (to) {
-//    this.dlg.style.opacity = to;
-//};
